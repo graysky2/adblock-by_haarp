39face1b65f62b5e99c3e0de276f2eedd7e205ba
graysky <graysky@archlinux.us>
2013-09-15 08:01:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## Adblock for TomatoUSB by haarp
+A clean, lean, and mean adblocking script.
+[Upstream URL](http://www.linksysinfo.org/index.php?threads/script-clean-lean-and-mean-adblocking.68464/)
+
+## Features
+* Takes public blocklists for known ad hosts and redirects them via DNS poisoning.
+* Pixelserv (optional) through a second router IP (Web GUI on port 80 still works)! Or if pixelserv is not desired, redirect to 0.0.0.0, which will also kill ads (but might produce error messages).
+* Does not interfere with normal dnsmasq operation.
+* Does not try to "optimize" it (that's what the "Custom configuration" box on the web GUI is for, people)!
+* Does not break Tomato's ability to restart dnsmasq should it crash.
+* Additional blocklist sources can easily be added.
+* Easy blacklist and whitelist.
+* Very optimized: Updates as quickly and with as little CPU/memory usage as possible.
+* Small and lean: Only does what it needs to do, then gets out of the way.
+* Readable code.
+
+## Setup Instructions
+Note -- Users of Adblock pre-v4.0 need to completely remove it.
+
+### 1. Setup the router
+* Verify that your Tomato supports custom dnsmasq configs (i.e. shows this line under Advanced->DHCP/DNS: "Note: The file /etc/dnsmasq.custom is also added to the end of Dnsmasq's configuration file if it exists.")
+* Set up some kind of non-volatile storage. This is up to you, options are JFFS, CIFS, SD card, USB and possibly more. Note the path.
+* Designate a directory on your storage for adblock, e.g. /jffs/adblock/ (as seen by the router). Avoid spaces! This is the PREFIX.
+
+### 2. Install Adblock
+* Place both files in this github repo on the filesystem of your target device. For example: /opt/adblock
+* Make adblock.sh executable `chmod +x /opt/adblock.sh`
+* Edit /opt/adblock/config adjusting the PREFIX variable to match that path chosen.
+* Optionally comment or uncomment the various sources in the config file per your perferences.
+
+## Running Adblock
+Users can call the script from the shell (or the TomatoUSB GUI under Tools>System>Execute System Commands) or a cronjob.
+
+## Command-line Options
+`/opt/adblock/adblock.sh`	Default, update and enable adblocker.
+`/opt/adblock/adblock.sh stop`	Disable the adblocker.
+`/opt/adblock/adblock.sh restart`	Restart adblocker e.g. for config changes and script updates.
+`/opt/adblock/adblock.sh force`	Force updating of filters, even in not updated.
+`/opt/adblock/adblock.sh toggle`	Disable the adlocker if active, enable if inactive. Perfect for one of the custom buttons on your router.
+
+### Example starting the script when the router boot
+Administration>Scripts>WAN Up
+`[[ -x /opt/adblock/adblock.sh ]] && /opt/adblock/adblock.sh &`
+
+### Example updating once per week
+Administration>Scheduler>Custom x
+`[[ -x /opt/adblock/adblock.sh ]] && /opt/adblock/adblock.sh restart`
+
+### Notes
+* The script, pixelserv and all data reside on PREFIX. If you have other means of accessing that storage, those will probably be more convenient than pasting into boxes on the web GUI.
+* The script will automatically block anything bound for the pixelserv IP that is not intended for pixelserv itself.
+* Subsequent updates will only fetch blocklists that have changed, this however only works when the source server runs http on port 80.
+* You will need 2x-3x as much non-volatile storage as all filters combined. If your storage is too small, set RAMLIST to 1. Obviously, you now need enough free RAM to hold the filters instead! Additionally, the script will now have to redownload the filters when the router reboots.
+* If you change settings or update the script, please run the script with the restart option afterwards!
+* If you experience problems, check the router logs!
---
+++ adblock.sh
@@ -0,0 +1,189 @@
+#!/bin/sh
+## Clean, Lean and Mean Adblock v4.5 by haarp
+##
+## Options:
+## 'force': force updating sources,
+## 'stop': disable Adblock, 'toggle': quickly toggle Adblock on and off
+## 'restart': restart Adblock (e.g. for config changes)
+
+## TODO: 'clean', 'status' options
+
+alias elog='logger -t ADBLOCK -s'
+alias iptables='/usr/sbin/iptables'
+
+pidfile=/var/run/adblock.pid
+kill -0 $(cat $pidfile 2>/dev/null) &>/dev/null && {
+elog "Another instance found ($pidfile), exiting!"
+exit 1
+}
+echo $$ > $pidfile
+
+pexit() {
+	elog "Exiting"
+	rm $pidfile
+	exit $@
+}
+
+stop() {
+	elog "Stopping"
+	rm "$CONF" &>/dev/null
+
+	iptables -D INPUT -i $BRIDGE -p tcp -d $redirip --dport 80 -j ACCEPT &>/dev/null
+	killall pixelserv &>/dev/null
+	ifconfig $BRIDGE:1 down &>/dev/null
+	iptables -D INPUT -i $BRIDGE -p all -d $redirip -j DROP &>/dev/null
+
+	elog "Done, restarting dnsmasq"
+	service dnsmasq restart
+}
+
+grabsource() {
+	local host=$(echo $1 | awk -F"/" '{print $3}')
+	local path=$(echo $1 | awk -F"/" '{print substr($0, index($0,$4))}')
+	local lastmod=$(echo -e "HEAD /$path HTTP/1.1\r\nHost: $host\r\n\r\n" | nc -w30 $host 80 | tr -d '\r' | grep "Last-Modified")
+
+	local lmfile="$listprefix/lastmod-$(echo $1 | md5sum | cut -c 1-8)"
+	local sourcefile="$listprefix/source-$(echo $1 | md5sum | cut -c 1-8)"
+
+	[ "$force" != "1" -a -e "$sourcefile" -a -n "$lastmod" -a "$lastmod" == "$(cat "$lmfile" 2>/dev/null)" ] && {
+	elog "Unchanged: $1 ($lastmod)"
+	echo 2 >>"$listprefix/status"
+	return 2
+}
+
+(
+if wget $1 -O -; then
+	[ -n "$lastmod" ] && echo "$lastmod" > "$lmfile"
+	echo 0 >>"$listprefix/status"
+else
+	elog "Failed: $1"
+	echo 1 >>"$listprefix/status"
+fi
+) | tr -d "\r" | sed -e '/^[[:alnum:]:]/!d' | awk '{print $2}' | sed -e '/^localhost$/d' > "$sourcefile"
+}
+
+confgen() {
+	elog "Generating $listprefix/blocklist"
+	rm "$CONF" &>/dev/null
+
+	if [ -e "$prefix/whitelist" ]; then
+		sort -u "$listprefix"/source-* | grep -v -f "$prefix/whitelist" > "$listprefix/blocklist"
+	else
+		sort -u "$listprefix"/source-* > "$listprefix/blocklist"
+	fi
+	for w in $WHITELIST; do
+		sed -i -e "/$w/d" "$listprefix/blocklist"
+	done
+
+	[ -e "$prefix/blacklist" ] && {
+	cat "$prefix/blacklist" >> "$listprefix/blocklist"
+}
+for b in $BLACKLIST; do
+	echo "$b" >> "$listprefix/blocklist"
+done
+
+sed -i -e '/^$/d' -e "s:^:address=/:" -e "s:$:/$redirip:" "$listprefix/blocklist"
+
+elog "Config generated, $(wc -l < "$listprefix/blocklist") unique hosts to block"
+}
+
+prefix="$(cd "$(dirname "$0")" && pwd)"
+[ -e "$prefix/config" ] || {
+elog "$prefix/config not found!"
+pexit 11
+}
+cd "$prefix"	#ensure that config has the correct path
+source "config"
+
+if [ "$RAMLIST" == "1" ]; then
+	listprefix="/var/lib/adblock"
+	[ -d "$listprefix" ] || mkdir "$listprefix"
+else
+	listprefix="$prefix"
+fi
+if [ "$PIXEL_IP" == "0" ]; then
+	redirip="0.0.0.0"
+else
+	[ -x "$prefix/pixelserv" ] || {
+	elog "$prefix/pixelserv not found/executable!"
+	pexit 10
+}
+redirip=$(ifconfig $BRIDGE | awk '/inet addr/{print $3}' | awk -F":" '{print $2}' | sed -e "s/255/$PIXEL_IP/")
+fi
+
+
+case "$1" in
+	restart) stop;;
+stop)	stop; pexit 0;;
+	toggle)	[ -e "$CONF" ] && { stop; pexit 0; };;
+force)	force="1";;
+	"")	:;;
+*)	elog "'$1' not understood!"; pexit 1;;
+esac
+
+
+elog "Download starting"
+until ping -q -c1 google.com >/dev/null; do
+	elog "Waiting for connectivity..."
+	sleep 30
+done
+
+trap 'elog "Signal received, cancelling"; rm "$listprefix"/source-* &>/dev/null; rm "$listprefix"/lastmod-* &>/dev/null; pexit 130' SIGQUIT SIGINT SIGTERM SIGHUP
+
+echo -n "" >"$listprefix/status"
+for s in $SOURCES; do
+	grabsource $s &
+done
+wait
+
+while read ret; do
+	case "$ret" in
+		0)	downloaded=1;;
+	1)	failed=1;;
+2)	unchanged=1;;
+	esac
+done < "$listprefix/status"
+rm "$listprefix/status"
+
+trap - SIGQUIT SIGINT SIGTERM SIGHUP
+
+if [ "$downloaded" == "1" ]; then
+	elog "Downloaded"
+	confgen
+elif [ "$unchanged" == "1" ]; then
+	elog "Filters unchanged"
+	if [ ! -e "$listprefix/blocklist" ]; then
+		confgen
+	elif [ ! -e "$CONF" ]; then #needlink
+		:
+	else pexit 2
+	fi
+else
+	elog "Download failed"
+	if [ -e "$listprefix/blocklist" -a ! -e "$CONF" ]; then #needlink
+		:
+	else pexit 3
+	fi
+fi
+
+echo "conf-file=$listprefix/blocklist" > "$CONF"
+
+if [ "$PIXEL_IP" != "0" ]; then
+	if ps | grep -v grep | grep -q "$prefix/pixelserv $redirip"; then
+		elog "pixelserv already running, skipping"
+	else
+		elog "Setting up pixelserv on $redirip"
+
+		iptables -vL INPUT | grep -q "$BRIDGE.*$redirip *tcp dpt:www" || {
+		iptables -I INPUT -i $BRIDGE -p all -d $redirip -j DROP
+		iptables -I INPUT -i $BRIDGE -p tcp -d $redirip --dport 80 -j ACCEPT
+	}
+	ifconfig $BRIDGE:1 $redirip up
+	"$prefix/pixelserv" $redirip $PIXEL_OPTS
+fi
+fi
+
+elog "Done, restarting dnsmasq"
+service dnsmasq restart
+
+pexit 0
